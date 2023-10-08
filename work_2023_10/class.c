8c6c4551a594b2dd3b651d0e202c5d962b05cdcf
Fredff1 <fuzhice618227@gmail.com>
2023-10-08 14:13:13 +0800

    OK

--- work_2023_10/class.c
+++ work_2023_10/class.c
@@ -3,8 +3,10 @@ int main(){
     float a,b;
     a=123456.789e5;
     b=a+20;
-    if(a==b){
+    if(abs(a-b)<1e-10){
         printf("y");
+    }else{
+        printf("n");
     }
     return 0;
 }
